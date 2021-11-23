275b4d73759349d12b62c0e1df939a5b546fe233
shelbyneil <72533224+shelbyneil@users.noreply.github.com>
2021-11-22 20:59:47 -0500

    Assignment 7 Completed finally LOL

--- learning-angular/src/app/create-component/create-component.component.ts
+++ learning-angular/src/app/create-component/create-component.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Content} from "../../../helper-files/content-interface";
 import {ContentService} from "../services/content.service";
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-create-component',
@@ -8,20 +9,54 @@ import {ContentService} from "../services/content.service";
   styleUrls: ['./create-component.component.css']
 })
 export class CreateComponentComponent implements OnInit {
+  @Output() addContent = new EventEmitter<Content>();
+  @Output() updateContent = new EventEmitter<Content>();
+  newBookItem: Content;
+  tempId!: string;
+  tempTag!: string;
 
-  constructor(private contentService: ContentService) { }
+  constructor(private contentService: ContentService, private messageService: MessageService) {
+    this.newBookItem = {
+      author: '',
+      title: '',
+      body: '',
+      type: '',
+    };
+  }
+
+
 
   ngOnInit(): void {
   }
 
-  @Output() newBookItem = new EventEmitter<Content>();
-
   CreateNewContent(): void{
-    this.contentService.addContent(this.newBookItem).subscribe(content => this.bookService.push(content));
+    this.newBookItem.tags = this.tempTag.split(",");
+    this.contentService.addContent(this.newBookItem).subscribe(newBookItem => {
+     this.messageService.add("Added content has an ID of: " + newBookItem.id);
+      this.newBookItem = {
+        author: '',
+        title: '',
+        body: ''
+      };
+      this.tempTag = "";
+      this.addContent.emit(newBookItem);
+    });
   }
 
   update(): void {
-    this.contentService.updateContent(this.newBookItem).subscribe(() => console.log("Content updated"));
+    this.newBookItem.tags = this.tempTag.split(",");
+    this.newBookItem.id = parseInt(this.tempId);
+    this.contentService.updateContent(this.newBookItem).subscribe(() => {
+      this.messageService.add("Updated content at ID: " + this.newBookItem.id);
+      this.tempTag = "";
+      this.tempId = "";
+      this.updateContent.emit(this.newBookItem);
+      this.newBookItem = {
+        author: '',
+        title: '',
+        body: ''
+      };
+    });
   }
 
 }
